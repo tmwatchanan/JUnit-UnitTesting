3b1f9b57cb4e7c45d90dcdf08ab9f7c60719718c
tmwatchanan <tmwatchanan@gmail.com>
2017-04-14 18:47:26 +0700

    + Created all unit test files and test suite ! Edited header in all files

--- src/cpe361/TestRunner.java
+++ src/cpe361/TestRunner.java
@@ -4,12 +4,14 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-/**
- * Created by Tommie on 14/4/2560.
- */
-public class TriangleRunner {
+/*
+* Project: UnitTesting
+* Created by Tommie on April 14, 2560 (14/4/2560).
+*/
+
+public class TestRunner {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(TriangleTest.class);
+        Result result = JUnitCore.runClasses(TestSuite.class);
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
